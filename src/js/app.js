1b6e4b4340438436a87da15655e55dec84461834
Mikhail <fregat59-mk@yandex.ru>
2022-04-13 06:50:19 +0500

    webp-images

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+// Подключение слайдера Swiper
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
